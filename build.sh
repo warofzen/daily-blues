3119ee2fe52231ab79b747e8ccd12c13b8c7a07b
mham <warofzen1@proton.me>
2024-12-26 09:05:30 +0700

    try it and see

--- build.sh
+++ build.sh
@@ -4,20 +4,6 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-
-### Install packages
-
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
-
-# this installs a package from fedora repos
-rpm-ostree install screen
-
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# Install [keyd](https://github.com/rvaiya/keyd)
+wget https://copr.fedorainfracloud.org/coprs/alternateved/keyd/repo/fedora-"$RELEASE"/alternateved-keyd-fedora-"$RELEASE".repo -O /etc/yum.repos.d/_copr:alternateved:keyd.repo
+rpm-ostree install keyd
