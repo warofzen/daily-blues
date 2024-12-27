8171aaa70a8075a98de83dc5962916ab7eec392b
mham <warofzen1@proton.me>
2024-12-27 10:25:20 +0700

    ulauncher

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# daily-blues
+# [daily-blues](https://www.youtube.com/watch?v=2NE5JO7-DIo)
 
 slightly edited bluefin
 
 list of changes:
-- add keyd
+- add [keyd](https://github.com/rvaiya/keyd)
+- add [ulauncher](https://ulauncher.io/)
--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ set -ouex pipefail
 
 RELEASE="$(rpm -E %fedora)"
 
-# Install [keyd](https://github.com/rvaiya/keyd)
+# Install keyd
 wget https://copr.fedorainfracloud.org/coprs/alternateved/keyd/repo/fedora-"$RELEASE"/alternateved-keyd-fedora-"$RELEASE".repo -O /etc/yum.repos.d/_copr:alternateved:keyd.repo
 rpm-ostree install keyd
+
+# Install ulauncher
+rpm-ostree install ulauncher
