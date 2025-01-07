6674df1d2f350c64a5f632bde54f32679ba38645
mham <warofzen1@proton.me>
2025-01-07 08:45:37 +0700

    bootc again?

--- build.sh
+++ build.sh
@@ -2,11 +2,9 @@
 
 set -ouex pipefail
 
-RELEASE="$(rpm -E %fedora)"
-
 # Install keyd
-wget https://copr.fedorainfracloud.org/coprs/alternateved/keyd/repo/fedora-"$RELEASE"/alternateved-keyd-fedora-"$RELEASE".repo -O /etc/yum.repos.d/_copr:alternateved:keyd.repo
-rpm-ostree install keyd
+dnf copr enable alternateved/keyd
+dnf install keyd -y
 
 # Install ulauncher
-rpm-ostree install ulauncher
+dnf install -y ulauncher
